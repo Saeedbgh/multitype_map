5489c9e5180976a84bc14e59ae763086b8f066aa
saeed <saeed.bagheri1983@gmail.com>
2025-03-08 17:55:23 +0330

    some edit for performance

--- App.java
+++ App.java
@@ -1,22 +1,11 @@
-import java.util.Objects;
-
 public class App {
     public static void main(String[] args) {
-        MultiTypeHashMap<String> map = new MultiTypeHashMap<>();
+        MultiTypeHashMap<String> map1 = new MultiTypeHashMap<>();
+        map1.put("B","A");
+        System.out.println(map1.get(map1));
 
-        map.put("key1", 42);           // ذخیره مقدار Integer
-        map.put("key2", "Hello");      // ذخیره مقدار String
-        map.put("key3", 3.14);         // ذخیره مقدار Double
+        String value1 = map1.get("B", String.class);
+        System.out.println(value1);
 
-        // آزمایش متد get با تایپ‌های مختلف
-        Integer intValue = map.get("key1", Integer.class);
-        String strValue = map.get("key2", String.class);
-        Double doubleValue = map.get("key3", Double.class);
-        String invalidValue = map.get("key1", String.class); // باید null بازگرداند
-
-        System.out.println("Integer value: " + intValue);   // خروجی: 42
-        System.out.println("String value: " + strValue);     // خروجی: Hello
-        System.out.println("Double value: " + doubleValue);  // خروجی: 3.14
-        System.out.println("Invalid value: " + invalidValue); // خروجی: null
     }
 }
